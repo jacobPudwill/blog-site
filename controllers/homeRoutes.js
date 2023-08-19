088a8b5c135f02d91ce2bc8cfdbaa31600db86f1
Jacob Pudwill <jacobpudwill@gmail.com>
2023-08-19 16:27:48 -0500

    Setup /dashboard for logged in users.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User, 
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
         res.render('homepage', {
+            posts,
             logged_in: req.session.logged_in
         });
     } catch (err) {
@@ -11,9 +24,18 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
+    const postData = await Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
     res.render('homepage', {
         layout: 'alt',
+        posts,
         logged_in: req.session.logged_in
     });
 });
