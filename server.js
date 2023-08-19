64f15dc61fd3dda145008453a2bd64cc32577bf9
Jacob Pudwill <jacobpudwill@gmail.com>
2023-08-19 02:22:08 -0500

    Set up cookies.

--- server.js
+++ server.js
@@ -6,11 +6,29 @@ const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
